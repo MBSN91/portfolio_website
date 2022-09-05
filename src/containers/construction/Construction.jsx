c0e3ebbabf3fc0e6230c8d6cacb41814a8eac98c
“Musa <hello@musabaysan.nlcleargit config --global user.name Musa Baysangit config --global user.name Musa Baysangit config --global user.email hello@musabaysan.nl>
2022-09-05 23:43:04 +0200

    button added

--- src/containers/construction/Construction.jsx
+++ src/containers/construction/Construction.jsx
@@ -6,7 +6,8 @@ const Construction = () => {
   return (
     <div className='mbsn__container'>
         <img className='logo' src={logo} alt='logo' />
-        <p className='paragraph'>website is rebuild from the ground up in React. <br />Portfolio website will be back soon!</p>
+        <p className='paragraph'>website is rebuild from the ground up in React. <br />Portfolio website will be back soon! <br />My old portfolio website can still be access with the button below!</p>
+        <button className='button' type='button'>Watch my earlier portfolio</button>
     </div>
   )
 }
