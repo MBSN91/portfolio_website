19acb6c0c54a7440349fc8a0a61028b47ea17738
“Musa <hello@musabaysan.nlcleargit config --global user.name Musa Baysangit config --global user.name Musa Baysangit config --global user.email hello@musabaysan.nl>
2022-09-05 23:57:07 +0200

    button is with link and poitner

--- src/containers/construction/Construction.jsx
+++ src/containers/construction/Construction.jsx
@@ -6,8 +6,8 @@ const Construction = () => {
   return (
     <div className='mbsn__container'>
         <img className='logo' src={logo} alt='logo' />
-        <p className='paragraph'>website is rebuild from the ground up in React. <br />Portfolio website will be back soon! <br />My old portfolio website can still be access with the button below!</p>
-        <button className='button' type='button'>Watch my earlier portfolio</button>
+        <p className='paragraph'>Portfolio is completly rebuild in React. <br />It will be back soon! <br /><p className='p6'>My old portfolio website(backup) is still online</p></p>
+        <a href='https://mbsn.webflow.io/'><button className='button' type='button'>Visit my earlier portfolio</button></a>
     </div>
   )
 }
