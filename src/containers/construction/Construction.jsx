c5bbba0b016290a1f808f3c8fdedf397774ce859
“Musa <hello@musabaysan.nlcleargit config --global user.name Musa Baysangit config --global user.name Musa Baysangit config --global user.email hello@musabaysan.nl>
2022-09-06 10:20:35 +0200

    small tweak in button UI

--- src/containers/construction/Construction.jsx
+++ src/containers/construction/Construction.jsx
@@ -6,7 +6,7 @@ const Construction = () => {
   return (
     <div className='mbsn__container'>
         <img className='logo' src={logo} alt='logo' />
-        <p className='paragraph'>Portfolio is completly rebuild in React. <br />It will be back soon! <br /><p className='p6'>My old portfolio website(backup) is still online</p></p>
+        <p className='paragraph'>Portfolio is being rebuild from scratch, using React <br />It will be back soon! <br /><p className='p6'>My old portfolio website(backup) is still online</p></p>
         <a href='https://mbsn.webflow.io/'><button className='button' type='button'>Visit my earlier portfolio</button></a>
     </div>
   )
