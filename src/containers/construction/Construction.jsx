9b3109c0369b06bb22d5de0669efab5fe5f15710
“Musa <hello@musabaysan.nlcleargit config --global user.name Musa Baysangit config --global user.name Musa Baysangit config --global user.email hello@musabaysan.nl>
2022-09-07 22:32:08 +0200

    announcement banner and photo added

--- src/containers/construction/Construction.jsx
+++ src/containers/construction/Construction.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../../assets/logo.svg';
+import musa from '../../assets/musa-baysan.png'
 import './construction.css';
 import Cta from '../../components/cta/Cta';
 
@@ -8,16 +9,17 @@ const Construction = () => {
   return (
     <div className='mbsn--container'>
       <div className='box'>
+        <img className='mbsn--selfie' src={musa} alt='musabaysan' />
         <div className='boxje1' />
         <div className='boxje2' />
         <div className='boxje3' />
         <div className='boxje4' />
         <div className='mbsn--box'>
           <img className='mbsn--logo' src={logo} alt='logo' />
-          <p className='mbsn--paragraph'>Portfolio is being rebuild from scratch <br />It will be back soon! <br /><p className='p6'>My old portfolio website(backup) is still online</p></p>
-          <a href='https://mbsn.webflow.io/'>
+          <p className='mbsn--paragraph'>Portfolio is being rebuild from scratch <br />It will be back soon!</p>
+          <div className='mbsn--btnannounce'><a href='https://mbsn.webflow.io/'>
             <Cta>visit my backup portfolio</Cta>
-          </a>
+          </a><div className='mbsn--announce'>My old portfolio website is still online!</div></div>
         </div>
       </div>
     </div>
