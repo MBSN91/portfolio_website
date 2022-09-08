55b457ff027738f8ea7c6852ce18024687a33711
“Musa <hello@musabaysan.nlcleargit config --global user.name Musa Baysangit config --global user.name Musa Baysangit config --global user.email hello@musabaysan.nl>
2022-09-08 21:23:26 +0200

    added button animation in the component CTA

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './cta.css';
+import arrow from '../../assets/arrow.svg'
 
 export const Cta = ({
   children
 }) => {
   return (
     <button className='mbsn--primary--btn'>
-      {children}
+      {children} <img className='arrow' src={arrow} alt='arrow pointing right' />
     </button>
   )
 }
